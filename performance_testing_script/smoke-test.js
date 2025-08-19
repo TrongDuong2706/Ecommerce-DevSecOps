a514e55a9fd705a6679b79c7a8d677bb44ee827a
Duong Van Trong <trongdeptrai57@gmail.com>
2025-08-19 08:22:22 +0000

    Update performance_testing_script/smoke-test.js

--- performance_testing_script/smoke-test.js
+++ performance_testing_script/smoke-test.js
@@ -1,12 +1,13 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-export let options = {
+export const options = {
     vus: 1,
     duration: '10s',
 };
 
 export default function () {
+    // NOSONAR
     const BASE_URL = 'http://192.168.252.161:3000/';
     let res = http.get(BASE_URL);
     check(res, {
